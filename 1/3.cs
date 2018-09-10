d2dc1dff53eb997db738d364f44a06daa1be16ed
Alex147 <tula147@yandex.ru>
2018-09-10 17:24:50 +0300

    Add 1 and 2

---
+++ 1/3.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightSequence {
+    public class Program {
+        static void Main(string[] args) {
+            string[] buf = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            int n = int.Parse(buf[1]);
+            int size = int.Parse(buf[0]);
+
+            Queue<Package> queue = new Queue<Package>();
+            int currentWorkTime = 0;
+            int[] result = new int[n];
+            for (int i = 0; i < n; ++i) {
+                buf = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                Package package = new Package(int.Parse(buf[0]), int.Parse(buf[1]), i);
+
+                while (currentWorkTime <= package.Arrival && queue.Count > 0) {
+                    Package p = queue.Dequeue();
+                    result[p.i] = currentWorkTime;
+                    currentWorkTime += p.Duration;
+                }
+
+                if (currentWorkTime <= package.Arrival){
+                    currentWorkTime = package.Arrival;
+                    queue.Enqueue(package);
+                    continue;
+                }
+                if (queue.Count + 1 < size)
+                    queue.Enqueue(package);
+                else
+                    result[i] = -1;
+            }
+            while(queue.Count != 0) {
+                Package p = queue.Dequeue();
+                if (currentWorkTime < p.Arrival)
+                    currentWorkTime = p.Arrival;
+                result[p.i] = currentWorkTime;
+                currentWorkTime += p.Duration;
+            }
+            for (int i = 0; i < n; ++i)
+                Console.Write(result[i] + " ");
+            Console.ReadKey();
+        }
+    }
+
+    public class Package {
+        public int Arrival;
+        public int Duration;
+        public int i; 
+
+        public Package(int arrival, int duration, int i) {
+            Arrival = arrival;
+            Duration = duration;
+            this.i = i;
+        }
+    }
+}
---
+++ 2/1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightSequence {
+    public class Program {
+        static List<int[]> result = new List<int[]>();
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            string[] buf = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] heap = new int[n];
+            for (int i = 0; i < n; ++i)
+                heap[i] = int.Parse(buf[i]);
+            int size = 1;
+            while (2 * size < n)
+                size *= 2;
+            for (int i = size - 1; i >= 0; --i)
+                SiftDown(heap, i);
+
+            Console.WriteLine(result.Count);
+            foreach (int[] pair in result)
+                Console.WriteLine(pair[0] + " " + pair[1]);
+            Console.ReadKey();
+        }
+
+        static void SiftDown(int[] heap, int i) {
+            int min = heap[i];
+            int mini = i;
+
+            if (2 * i + 1 < heap.Length && heap[2 * i + 1] < min) {
+                mini = 2 * i + 1;
+                min = heap[2 * i + 1];
+            }
+
+            if (2 * i + 2 < heap.Length && heap[2 * i + 2] < min) {
+                mini = 2 * i + 2;
+                min = heap[2 * i + 2];
+            }
+
+            if (mini != i) {
+                result.Add(new int[] { i, mini });
+                int tmp = heap[i];
+                heap[i] = heap[mini];
+                heap[mini] = tmp;
+                SiftDown(heap, mini);
+            }
+        }
+    }
+}
